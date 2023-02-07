2a53fa2ac7875f9b7edfed666d6da029957a7ca5
Moelmi89 <moelmi89@gmail.com>
2023-02-07 18:30:07 +0000

    minor updates to css, js and html

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
+const MAX_DAILY_FORECAST = 5;
+
+var searchButton = document.getElementById("search-button");
+var location = document.getElementById("location");
+
+
 function getLocation(event)
 
 function onClickSearchButton(e)
