f038d133403a6265bdd5aa65d0a947f848f95ef1
Moelmi89 <moelmi89@gmail.com>
2023-03-03 20:39:04 +0000

    js updated search function

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,29 @@
-const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
-const MAX_DAILY_FORECAST = 5;
+const apiKey = '8c6e6a5f4bba093bda958cb2224e264b';
 
-var searchButton = document.getElementById("search-button");
-var location = document.getElementById("location");
-
+const searchButton = document.getElementById("search-button");
+const searchLocation = document.getElementById("location");
+const error = document.getElementById("error");
+const cityName = document.getElementById("location-name");
+const weatherDate = document.getElementById("weather-date");
+const temperature = document.getElementById("temperature");
+const windSpeed = document.getElementById("wind-speed");
+const weatherHumidity = document.getElementById("humidity");
+const weatherIcon = document.getElementById("weather-icon");
+const forecastDiv = document.getElementById("forecast");
+const recent = document.getElementById("recent-locations");
 
 function getLocation(event)
 
-function onClickSearchButton(e)
+function onClickSearchButton(e) {
+    const city = searchLocation.value;
+	if (city === "") {
+		error.textContent = "City name is required"
+		return;
+	}
+	dataFetch(city);
+	loadRecent();
+}
+
 
 function lookupLocation(search)
 
